b9b841abc0fad54a7fdc10c4c7d61bc17721d122
michealjack1 <42755185+michealjack1@users.noreply.github.com>
2018-10-26 16:25:02 +0200

    Update GitTest.java

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -16,7 +16,6 @@ public class GitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
         System.err.println("gggggggggg");
     }
     
